024a89cd2badb01315ada0ea1442baae70339590
Amira-GH <amiraghraiy@gmail.com>
2021-06-14 21:50:17 +0300

    step 3

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ app.get('/', (req, res) => {
   res.send('ok')
 })
 
+
+app.get('/test', (req, res) => {
+    res.send({status:200, message:"ok"})
+})
+
+app.get('/time', (req, res) => {
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes();
+    var dateTime = time;
+    res.send({status:200, time: dateTime})
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
