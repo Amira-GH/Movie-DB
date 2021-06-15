ac2123f1a797714ba42e5605d458310a734466dc
Amira-GH <amiraghraiy@gmail.com>
2021-06-15 10:52:26 +0300

    step 4

--- index.js
+++ index.js
@@ -19,6 +19,28 @@ app.get('/time', (req, res) => {
     res.send({status:200, time: dateTime})
 })
 
+
+app.get('/hello/:id', (req, res) => {
+    var id = req.params.id;
+    res.send({status:200, message:"Hello, " + id})    
+    
+})
+
+app.get('/hello', (req, res) => {
+    var id = req.params.id;
+    res.send({status:200, message:"Hello"})
+})
+
+app.get('/search', (req, res) => {
+    var Search = req.params.Search;
+    res.send({status:500, error:true, message:"you have to provide a search"})
+})
+
+app.get('/search/:search', (req, res) => {
+    var search = req.params.search;
+    res.send({status:200, message:"ok", data: search})
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
